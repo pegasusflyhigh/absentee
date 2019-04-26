90f2b1ce048094ebaa57fa2e8400437f0a24d9b7
Bhuvana Prabhu <bhuvna@joshsoftware.com>
2019-04-26 22:36:19 +0530

    Removed user role

--- db/migrate/20190426170535_add_user_fields.rb
+++ db/migrate/20190426170535_add_user_fields.rb
@@ -1,6 +1,5 @@
-class AddFieldsToUser < ActiveRecord::Migration[5.2]
+class AddUserFields < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :role, :string
     add_column :users, :name, :string
     add_column :users, :mobile_number, :string
     add_column :users, :active, :boolean
