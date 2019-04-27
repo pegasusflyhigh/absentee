8074464455d7711e70ce9ecae5953af455f1a1ae
Raj Kamal Lashkari <raj@joshsoftware.com>
2019-04-27 12:35:12 +0530

    Student validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,8 @@
 class Student < ApplicationRecord
   belongs_to :standard
   has_many :attendance_entries
+
+  validates_presence_of :roll_number, :name, :standard_id
+  validates_format_of :primary_number, :with =>  /\d[0-9]\)*\z/ , :message => I18n.t('error.invalid', attribute: 'primary number')
+  validates_format_of :secondary_number, :with =>  /\d[0-9]\)*\z/ , :message => I18n.t('error.invalid', attribute: 'secondary number'), if: :secondary_number
 end
