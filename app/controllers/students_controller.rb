ba6bb708b3721994fc97a491566e99be7531b841
Raj Kamal Lashkari <raj@joshsoftware.com>
2019-04-27 11:53:48 +0530

    Student controller actions

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,54 @@
+class StudentsController < ApplicationController
+
+  before_action :load_school, :load_standard, :load_student
+
+  def index
+    if params[:search_item].present?
+      @students = @standard.students.where(["LOWER(name) LIKE ?", "%#{params[:search_item].downcase}%"]).page(params[:page]).per(10)
+    else
+      @students = @standard.students.order(:name).page(params[:page]).per(10)
+    end
+  end
+
+  def new
+    @student = @standard.students.new
+  end
+
+  def create
+    @student = @standard.students.build(student_params)
+    if @student.save
+      flash[:success] = I18n.t('created.success', model_name: 'Student')
+    else
+      flash[:error] = I18n.t('created.failure', model_name: 'Student')
+    end
+  end
+
+  def update
+    if @student.update_attributes(student_params)
+      flash[:success] = I18n.t('updated.success', model_name: 'Student')
+      redirect_to standard_students_path(@standard) and return
+    else
+      flash[:error] = I18n.t('updated.failure', model_name: 'Student')
+      render :edit and return
+    end
+  end
+
+  private
+
+  def load_school
+    @current_school = current_user.school
+  end
+
+  def load_standard
+    @standard = @current_school.standards.find params[:standard_id]
+  end
+
+  def load_student
+    @student = @standard.students.find params[:id]
+  end
+
+  def student_params
+    params.require(:student).permit(:name, :roll_number, :primary_number, :secondary_number, :preferred_language, :standard_id, :active)
+  end
+
+end
